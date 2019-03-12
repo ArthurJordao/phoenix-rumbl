b15caa69de7bbc8800050a6e94d56f8fd08d89e6
ArthurJordao <arthurbernardijordao@gmail.com>
2019-03-12 18:08:43 -0300

    Format files

--- test/controllers/video_controller_test.exs
+++ test/controllers/video_controller_test.exs
@@ -16,24 +16,27 @@ defmodule Rumbl.VideoControllerTest do
     user_video = insert_video(user, %{title: "funny cats"})
     other_video = insert_video(insert_user(%{username: "other"}), %{title: "another video"})
 
-    conn = get conn, video_path(conn, :index)
+    conn = get(conn, video_path(conn, :index))
     assert html_response(conn, 200) =~ ~r/Listing videos/
     assert String.contains?(conn.resp_body, user_video.title)
     refute String.contains?(conn.resp_body, other_video.title)
   end
 
   test "requires a user authentication on all actions", %{conn: conn} do
-    Enum.each([
-      get(conn, video_path(conn, :new)),
-      get(conn, video_path(conn, :index)),
-      get(conn, video_path(conn, :show, "123")),
-      get(conn, video_path(conn, :edit, "123")),
-      get(conn, video_path(conn, :update, "123", %{})),
-      get(conn, video_path(conn, :create, %{})),
-      get(conn, video_path(conn, :delete, "123")),
-    ], fn conn ->
-      assert html_response(conn, 302)
-      assert conn.halted
-    end)
+    Enum.each(
+      [
+        get(conn, video_path(conn, :new)),
+        get(conn, video_path(conn, :index)),
+        get(conn, video_path(conn, :show, "123")),
+        get(conn, video_path(conn, :edit, "123")),
+        get(conn, video_path(conn, :update, "123", %{})),
+        get(conn, video_path(conn, :create, %{})),
+        get(conn, video_path(conn, :delete, "123"))
+      ],
+      fn conn ->
+        assert html_response(conn, 302)
+        assert conn.halted
+      end
+    )
   end
 end
